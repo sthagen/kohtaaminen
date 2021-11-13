8987d8918eae6cc61d8cb417dd834360b952629a
Stefan Hagen <stefan@hagen.link>
2021-11-13 13:38:22 +0100

    Chores

--- tests/test_kohtaaminen.py
+++ tests/test_kohtaaminen.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import kohtaaminen.kohtaaminen as ko
 
+
+def test_something():
+    assert ko.main([], debug=False) == 0
