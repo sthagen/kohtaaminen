4d8ac331998c1c0d991c0231c46ab5cf7b2c945e
Stefan Hagen <stefan@hagen.link>
2021-11-13 13:33:45 +0100

    Happy linter, happy winter

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
-import logging
-
-import pytest  # type: ignore
-
 import kohtaaminen.cli as cli
 
 
 def test_main_ok_no_args(capsys):
     assert cli.main([], debug=False) == 0
     out, err = capsys.readouterr()
-    assert "ok" in out.lower()
+    assert 'ok' in out.lower()
     assert not err
