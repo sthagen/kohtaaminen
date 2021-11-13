98bd65dad3e8d5ece2abe63d35c225710aeddc4b
Stefan Hagen <stefan@hagen.link>
2021-11-13 12:09:29 +0100

    Update __main__.py

--- kohtaaminen/__main__.py
+++ kohtaaminen/__main__.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long,missing-module-docstring
+import sys
 
+from kohtaaminen.cli import app
+
+if __name__ == '__main__':
+    sys.exit(app(prog_name='kohtaaminen'))  # pragma: no cover
