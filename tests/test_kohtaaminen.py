5dc10a530f8f9da31f81a98a2c1ad16bfd020aa4
Stefan Hagen <stefan@hagen.link>
2021-11-13 17:45:29 +0100

    First working API test

--- tests/test_kohtaaminen.py
+++ tests/test_kohtaaminen.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pathlib
+
 import kohtaaminen.kohtaaminen as ko
 
 
 def test_something():
-    assert ko.main([], debug=False) == 0
+    inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'export.zip'))
+    assert ko.main(['translate', inp]) == 0
