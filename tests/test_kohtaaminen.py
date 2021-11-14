04ae4511faf0c58b02652f8904d2d35100399d18
Stefan Hagen <stefan@hagen.link>
2021-11-14 16:30:51 +0100

    Added test for request verification (too few artuments)

--- tests/test_kohtaaminen.py
+++ tests/test_kohtaaminen.py
@@ -8,3 +8,7 @@ import kohtaaminen.kohtaaminen as ko
 def test_ko_main():
     inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'export.zip'))
     assert ko.main(['translate', inp]) == 0
+
+
+def test_ko_verify_request_too_few():
+    assert ko.verify_request([1]) == (2, 'received wrong number of arguments', [''])
