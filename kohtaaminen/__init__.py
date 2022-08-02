88f31fe1004d775e9956d0977cd3617486442e95
Stefan Hagen <stefan@hagen.link>
2022-08-02 20:16:05 +0200

    Bumped impl version for release

--- kohtaaminen/__init__.py
+++ kohtaaminen/__init__.py
@@ -1,7 +1,7 @@
 """Meeting, rendezvous, confluence (Finnish: kohtaaminen) mark up, down, and up again."""
 # [[[fill git_describe()]]]
-__version__ = '2022.8.2+parent.4c17cf8f'
-# [[[end]]] (checksum: eaa209e5e7a90c217df25f8d1c12f751)
+__version__ = '2022.8.2+parent.39e50623'
+# [[[end]]] (checksum: 4893823d414c2d277f8cb05be925f1b3)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
